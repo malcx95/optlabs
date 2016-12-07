d16cb8643044db7e78305434b9753e848bbd25bb
Malcolm <malvi108@student.liu.se>
2016-12-07 16:03:47 +0100

    Done with prob3

--- lab2/prob3/kchessb.mod
+++ lab2/prob3/kchessb.mod
@@ -1,4 +1,4 @@
-# Chess
+# Chess (b)
 
 var x{1..8, 1..8} binary;
 
@@ -10,6 +10,18 @@ subject to row_cond{j in 1..8}:
 subject to col_cond{i in 1..8}:
     sum {j in 1..8} x[i, j] <= 1;
 
+subject to diag_cond1{k in 0..6}:
+	sum {i in 1..(8-k)} x[i+k,i] <= 1;
+
+subject to diag_cond2{k in 1..6}:
+	sum {i in 1..(8-k)} x[i,i+k] <= 1;
+
+subject to diag_cond3{k in 1..6}:
+	sum {i in 1..(8-k)} x[i,9-k-i] <= 1;
+
+subject to diag_cond4{k in 0..6}:
+	sum {i in 1..(8-k)} x[i+k,9-i] <= 1;
+
 solve;
 
 printf " *** Optimum: *** \n";
@@ -20,4 +32,3 @@ for {i in 1..8} {
 	}
 	printf "\n";
 }
-
---
+++ lab2/prob3/kchessb.sol
@@ -0,0 +1,157 @@
+Problem:    kchessb
+Rows:       43
+Columns:    64 (64 integer, 64 binary)
+Non-zeros:  316
+Status:     INTEGER OPTIMAL
+Objective:  total = 8 (MAXimum)
+
+   No.   Row name        Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 total                       8                             
+     2 row_cond[1]                 1                           1 
+     3 row_cond[2]                 1                           1 
+     4 row_cond[3]                 1                           1 
+     5 row_cond[4]                 1                           1 
+     6 row_cond[5]                 1                           1 
+     7 row_cond[6]                 1                           1 
+     8 row_cond[7]                 1                           1 
+     9 row_cond[8]                 1                           1 
+    10 col_cond[1]                 1                           1 
+    11 col_cond[2]                 1                           1 
+    12 col_cond[3]                 1                           1 
+    13 col_cond[4]                 1                           1 
+    14 col_cond[5]                 1                           1 
+    15 col_cond[6]                 1                           1 
+    16 col_cond[7]                 1                           1 
+    17 col_cond[8]                 1                           1 
+    18 diag_cond1[0]
+                                   0                           1 
+    19 diag_cond1[1]
+                                   1                           1 
+    20 diag_cond1[2]
+                                   1                           1 
+    21 diag_cond1[3]
+                                   0                           1 
+    22 diag_cond1[4]
+                                   1                           1 
+    23 diag_cond1[5]
+                                   1                           1 
+    24 diag_cond1[6]
+                                   0                           1 
+    25 diag_cond2[1]
+                                   1                           1 
+    26 diag_cond2[2]
+                                   1                           1 
+    27 diag_cond2[3]
+                                   0                           1 
+    28 diag_cond2[4]
+                                   1                           1 
+    29 diag_cond2[5]
+                                   1                           1 
+    30 diag_cond2[6]
+                                   0                           1 
+    31 diag_cond3[1]
+                                   1                           1 
+    32 diag_cond3[2]
+                                   1                           1 
+    33 diag_cond3[3]
+                                   1                           1 
+    34 diag_cond3[4]
+                                   1                           1 
+    35 diag_cond3[5]
+                                   0                           1 
+    36 diag_cond3[6]
+                                   0                           1 
+    37 diag_cond4[0]
+                                   1                           1 
+    38 diag_cond4[1]
+                                   1                           1 
+    39 diag_cond4[2]
+                                   0                           1 
+    40 diag_cond4[3]
+                                   1                           1 
+    41 diag_cond4[4]
+                                   0                           1 
+    42 diag_cond4[5]
+                                   0                           1 
+    43 diag_cond4[6]
+                                   1                           1 
+
+   No. Column name       Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 x[1,1]       *              0             0             1 
+     2 x[1,2]       *              0             0             1 
+     3 x[1,3]       *              0             0             1 
+     4 x[1,4]       *              0             0             1 
+     5 x[1,5]       *              1             0             1 
+     6 x[1,6]       *              0             0             1 
+     7 x[1,7]       *              0             0             1 
+     8 x[1,8]       *              0             0             1 
+     9 x[2,1]       *              0             0             1 
+    10 x[2,2]       *              0             0             1 
+    11 x[2,3]       *              0             0             1 
+    12 x[2,4]       *              0             0             1 
+    13 x[2,5]       *              0             0             1 
+    14 x[2,6]       *              0             0             1 
+    15 x[2,7]       *              1             0             1 
+    16 x[2,8]       *              0             0             1 
+    17 x[3,1]       *              0             0             1 
+    18 x[3,2]       *              1             0             1 
+    19 x[3,3]       *              0             0             1 
+    20 x[3,4]       *              0             0             1 
+    21 x[3,5]       *              0             0             1 
+    22 x[3,6]       *              0             0             1 
+    23 x[3,7]       *              0             0             1 
+    24 x[3,8]       *              0             0             1 
+    25 x[4,1]       *              0             0             1 
+    26 x[4,2]       *              0             0             1 
+    27 x[4,3]       *              0             0             1 
+    28 x[4,4]       *              0             0             1 
+    29 x[4,5]       *              0             0             1 
+    30 x[4,6]       *              1             0             1 
+    31 x[4,7]       *              0             0             1 
+    32 x[4,8]       *              0             0             1 
+    33 x[5,1]       *              0             0             1 
+    34 x[5,2]       *              0             0             1 
+    35 x[5,3]       *              1             0             1 
+    36 x[5,4]       *              0             0             1 
+    37 x[5,5]       *              0             0             1 
+    38 x[5,6]       *              0             0             1 
+    39 x[5,7]       *              0             0             1 
+    40 x[5,8]       *              0             0             1 
+    41 x[6,1]       *              1             0             1 
+    42 x[6,2]       *              0             0             1 
+    43 x[6,3]       *              0             0             1 
+    44 x[6,4]       *              0             0             1 
+    45 x[6,5]       *              0             0             1 
+    46 x[6,6]       *              0             0             1 
+    47 x[6,7]       *              0             0             1 
+    48 x[6,8]       *              0             0             1 
+    49 x[7,1]       *              0             0             1 
+    50 x[7,2]       *              0             0             1 
+    51 x[7,3]       *              0             0             1 
+    52 x[7,4]       *              0             0             1 
+    53 x[7,5]       *              0             0             1 
+    54 x[7,6]       *              0             0             1 
+    55 x[7,7]       *              0             0             1 
+    56 x[7,8]       *              1             0             1 
+    57 x[8,1]       *              0             0             1 
+    58 x[8,2]       *              0             0             1 
+    59 x[8,3]       *              0             0             1 
+    60 x[8,4]       *              1             0             1 
+    61 x[8,5]       *              0             0             1 
+    62 x[8,6]       *              0             0             1 
+    63 x[8,7]       *              0             0             1 
+    64 x[8,8]       *              0             0             1 
+
+Integer feasibility conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
